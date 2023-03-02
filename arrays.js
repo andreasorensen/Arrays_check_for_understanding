32960fc9998df5d8fc3a5a4056026912ed1c2932
Andrea Sorensen <dreasor2987@icloud.com>
2023-03-01 16:14:24 -0800

    Show array m ethod understanding.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
 var peopleILove = ["Parents", "Parnter", "Friends", "Family"];
 var numbers = [1, 2, 3, 4, 5];
 var boolean = [true, false, true, false];
+
+// This will remove the last element in the array (0)
+peopleILove.pop()
+console.log(peopleILove);
+
+// This will remove the element in the number 4 position
+console.log(numbers.slice(4));
+
+//This will remove the first element in the array.
+boolean.shift()
+console.log(boolean);
+
+// This will put back the first element in the array that was removed with the method shift
+boolean.unshift()
+console.log(boolean);
